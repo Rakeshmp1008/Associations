5c223e95d8783c536008a2d679076d84294ffb8d
RAKESH PARAMAGOND <paramagondrakeshxworkz@gmail.com>
2021-10-20 00:54:54 +0530

    Array and string array

---
+++ ArrayTask.java
@@ -0,0 +1,118 @@
+package com.rakesh.pack1;
+
+public class ArrayTask {
+
+	public static void main(String[] args) {
+			
+		//phoneNumber
+		long amruta=9565955424L;
+		long ashwini=7285265544L;
+		long[] phoneNumber= {9485625875L,9658426574L,6245871256L,
+							9264289515L,9154785624L,9785624865L,8525151515L
+							,6264985554L,amruta,ashwini};
+		for (int i = 0; i < phoneNumber.length; i++) {
+			
+			System.out.println(i);
+			System.out.println(phoneNumber[i]);
+			
+		}
+		
+	
+		//Aadhar Card Number
+		long rakesh=2546256832569L;
+		long satish=65855654454654L;
+		long[] aadharCard= {565688645698L,218546845466L,rakesh,satish,365214569874L,456874598456L,
+							456247852365L,546984751234L,265478549856L,254875319514L};
+		for (int i = 0; i < aadharCard.length; i++) {
+			
+			System.out.println(i);
+			System.out.println(aadharCard[i]);
+		}
+		
+		//Bank Account Number
+		int john=562452;
+		int jason=655978;
+		int kamal=569854;
+		int[] bankAccNo= {john,jason,kamal,556568,658945,235465,125476,359875,264826,346875};
+		
+		for (int i = 0; i < bankAccNo.length; i++) {
+			
+			System.out.println(bankAccNo[i]);
+			System.out.println(i);
+		}
+		
+		//pincode
+		int jamkhandi=587301;
+		
+		int vidhanaSoudha=560001;	
+		int vidyanagara=562157;	
+		int vidyaranyapura=560097;	
+		int vijayanagar=560040;
+		int[] pinCode= {560049,560004,560091,560018,560047,
+						vidhanaSoudha,vidyanagara,vidyaranyapura,vijayanagar};
+		
+		for (int i = 0; i < pinCode.length; i++) {
+			
+			System.out.println(i);
+			System.out.println(pinCode[i]);
+		}
+		
+		//order Id
+		int dosa=023;
+		int benneDosa=024;
+		int masalDosa=056;
+		int idli=021;
+		int[] orderNo= {012,054,045,047,065,014,dosa,benneDosa,masalDosa,idli};
+		
+		for (int i = 0; i < orderNo.length; i++) {
+			
+			System.out.println(i);
+			System.out.println(orderNo[i]);
+		}
+		
+		//MarksCard Number
+		long sachin=2013140256L;
+		long basu=2013140289L;
+		long naggappa=2013140568;
+		long[] marksCardNo= {2013145682L,2013147895L,2013145698L,2013142368L,
+							2013145795L,2013145791L,2013141514L,sachin,basu,naggappa};
+							
+		for (int i = 0; i < orderNo.length; i++) {
+			
+			System.out.println(i);
+			System.out.println(marksCardNo[i]);
+		
+		}
+		
+		//Roll Number
+		int[] rollNo= {1,2,3,4,5,6,7,8,9,10};
+		
+		for (int i = 0; i < rollNo.length; i++) {
+			
+			System.out.println(i);
+			System.out.println(rollNo[i]);
+		}
+		
+		//OTP
+		int[] otp= {1234,5847,6589,9856,8569,9658,5236,5482,6573,1573};
+		
+		for (int i = 0; i < otp.length; i++) {
+			
+			System.out.println(i);
+			System.out.println(otp[i]);
+			
+		}
+		
+		//Door No
+		int[] doorNo= {22,23,25,54,56,85,12,14,16,27};
+		
+		for (int i = 0; i < doorNo.length; i++) {
+			
+			System.out.println(i);
+			System.out.println(doorNo[i]);
+		}
+
+
+	}
+		
+}
